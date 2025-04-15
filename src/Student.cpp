2257d0eb8dc8cb740a6bc964c1a753f664daee5d
prothom-antor-banik <prothomantorbanik@gmail.com>
2025-04-15 15:16:42 +0600

    add function created

--- src/Student.cpp
+++ src/Student.cpp
@@ -4,6 +4,25 @@ Student::Student(){
     cout << "Student constructor" << endl;
 }
 
+Student* Student::getInstance(){
+    if(obj == nullptr) obj = new Student();
+    return obj;
+}
+
+string Student::add(string name){
+    studentList.push_back(name);
+}
+
+string Student::remove(string name){
+
+}
+
+string Student::display(string){
+
+}
+
 Student::~Student(){
     cout << "Student distructor" << endl;
 }
+
+Student* Student::obj = nullptr;
--- src/Student.h
+++ src/Student.h
@@ -4,8 +4,13 @@
 using namespace std;
 
 class Student{
+    static Student* obj;
+    vector<string> studentList;
 public:
     Student();
-    
+    static Student* getInstance();
+    string add(string);
+    string remove(string);
+    string display(string);
     ~Student();
 };
